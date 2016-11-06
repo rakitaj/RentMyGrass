bd5a86d59be07e10cbc735ed5422ffe96fb617a3
Jonah Dahlquist <jonah@nucleussystems.com>
2016-11-05 18:07:55 -0700

    Added facebook callback ctrl

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :listings
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get 'users/signup'
   get 'users/login'
 
