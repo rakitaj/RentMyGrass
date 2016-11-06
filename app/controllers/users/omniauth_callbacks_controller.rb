9b3d76254599043d9792a5ef60025b20893f8a05
Jonah Dahlquist <jonah@nucleussystems.com>
2016-11-05 18:11:26 -0700

    Fixed typos in callback ctrl

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallacksController
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
